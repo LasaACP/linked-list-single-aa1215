5e0174f456454fd4937677f3c6d0717fdcc0a9f1
Akshara Anand <aksharasanand@gmail.com>
2017-02-10 17:45:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,139 +1,187 @@
-//
-//  main.cpp
-//  LLTemplate
-//
-//  Created by James Shockey on 12/6/16.
-//  Copyright © 2016 James Shockey. All rights reserved.
-//
-
-#include <iostream>
-#include <fstream>
-#include <cmath> 
 #include "slist.h"
 
-using namespace std;
+/*
+Class Library File
+*/
 
-class Airport
-{
-public:
-    char   code[5];
-    double longitude;
-    double latitude;
-    
-};
-
-
-
-void simpleSortTotal(Airport* s[], int c);
-double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d);
-
-int main()
-{
-    ifstream infile;
-    int i=0;
-    char cNum[10] ;
-    Airport* airportArr[13500];
-    int   airportCount;
-    //Airport* a[13500];
-    
-    infile.open ("./USAirportCodes.csv", ifstream::in);
-    if (infile.is_open())
-    {
-        int   c=0;
-        while (infile.good())
-        {
-            airportArr[c] = new Airport();
-            infile.getline(airportArr[c]->code, 256, ',');
-            infile.getline(cNum, 256, ',');
-            airportArr[c]->longitude = atof(cNum);
-            infile.getline(cNum, 256, '\n');
-            airportArr[c]->latitude = atof(cNum);
-
-            if (!(c % 1000))
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-
-            /*
-            if (!(c % 1000))
-            {
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-                cout << airportArr[c+1]->code << endl; //" long: " << airportArr[c+1]->longitude << " lat: " << airportArr[c+1]->latitude <<  endl;                               
-            }
-            */
-
-            
-            i++ ;
-            c++;
+// Constructor
+Node(int val)
+    : _value(val), _pNext(NULL)
+    { }
+   
+  
+// Destructor
+while (head) {
+    nodeptr tmp=head->next;
+    delete head;
+    head = tmp;
+}
+// add(value)				//Adds a new value to the end of this list.
+template<typename T>
+void SLList<T>::add(const T& val) {
+    Node<T>* prev = head;
+    for (Node<T>* ptr = head->next; ptr; ptr = ptr->next) {
+        if (ptr->data == val) {
+            std::cout << "Duplicate: " << val << '\n';
+            return;
         }
-        airportCount = c-1;
-        infile.close();
-        
-         for (int c=0; c < airportCount; c++)
-            if (!(c % 1000))
-            {
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-                cout << airportArr[c+1]->code << " long: " << airportArr[c+1]->longitude << " lat: " << airportArr[c+1]->latitude <<  endl;
-                cout <<"Distance between " << airportArr[c]->code << " and " << airportArr[c+1]->code << " is "
-                  << distanceEarth( airportArr[c]->longitude, airportArr[c]->latitude , airportArr[c+1]->longitude, airportArr[c+1]->latitude) << endl;
-            }
-
-
-
+        prev = ptr;
     }
+    prev->next = new Node<T>(val);
+}
+// clear()					//Removes all elements from this list.
+template<typename C>
+void SLList<T>::clear(const T& val) {
+    Node<T>* prev = head;
+    for (Node<T>* ptr = head->next; ptr; ptr = ptr->next) {
+        if (ptr->data == val) {
+            std::cout << "Duplicate: " << val << '\n';
+            return;
+        }
+        prev = ptr;
+    }
+    prev->next = new Node<T>(val);
+}
+// equals(list)				//Returns true if the two lists contain the same elements in the same order.
+bool areIdentical(struct node *a, struct node *b)
+{
+    while (a != NULL && b != NULL)
+    {
+        if (a->data != b->data)
+            return false;
+        a = a->next;
+        b = b->next;
+    }
+    return (a == NULL && b == NULL);
+}
+//get(index)				//Returns the element at the specified index in this list.
+ int getValue(void)
+    { return _value; }
+    
+    Node* getNext(void)
+    { return _pNext; }
+};
+//insert(index, value)		//Inserts the element into this list before the specified index.
+void insert(int data, int pos)
+{
+  Node* prev = new Node();
+  Node* curr = new Node();
+  Node* newNode = new Node();
+  newNode->data = data;
+
+  int tempPos = 0;   // Traverses through the list
+
+  curr = head;      // Initialize current to head;
+  if(head != NULL)
+  {
+    while(curr->next != NULL && tempPos != pos)
+    {
+        prev = curr;
+        curr = curr->next;
+        tempPos++;
+    }
+    if(pos==0)
+    {
+       cout << "Adding at Head! " << endl;
+       // Call function to addNode from head;
+    }
+    else if(curr->next == NULL && pos == tempPos+1)
+    {
+      cout << "Adding at Tail! " << endl;
+      // Call function to addNode at tail;
+    }
+    else if(pos > tempPos+1)
+      cout << " Position is out of bounds " << endl;
+     //Position not valid
+
     else
     {
-        cout << "Error opening file";
+        prev->next = newNode;
+        newNode->next = curr;
+        cout << "Node added at position: " << pos << endl;
     }
- 
-
-
-   // Initialize Linked List
-   List<Double> list = new LinkedList<Double>(Arrays.asList(1.2,1.3,3.2));
-   // 
-
-    
+ }
+ else
+ {
+    head = newNode;
+    newNode->next=NULL;
+    cout << "Added at head as list is empty! " << endl;
+ }
 }
-
-
-
-#define pi 3.14159265358979323846
-#define earthRadiusKm 6371.0
-
-// This function converts decimal degrees to radians
-double deg2rad(double deg) {
-  return (deg * pi / 180);
+//exchg(index1, index2)		//Switches the payload data of specified indexex.
+switch ( input ) {
 }
-
-//  This function converts radians to decimal degrees
-double rad2deg(double rad) {
-  return (rad * 180 / pi);
+// isEmpty()				//Returns true if this list contains no elements.
+int isEmpty( node * list )
+ {
+   if( !list )
+      return 1;
+   return 0;
+ }
+// remove(index)			//Removes the element at the specified index from this list.
+void removeElement(int remValue) {
+    LinkedList* prev = head;
+    LinkedList* current = head->pNextValue;
+    while(current != NULL) {
+        if(current->value == remValue) { 
+            break; 
+        }
+        else {
+            cout << "Value " << current->value << " does not match " << remValue << ".\n";
+            prev = current; 
+            current = current->pNextValue; 
+        }
+    }
+    if(current == NULL) { 
+        cout << "Can't remove value: no match found.\n"; 
+    } else {
+        cout << "Deleting: " << current << "\n";
+        prev->pNextValue = current->pNextValue; 
+        delete current; 
+    }
 }
-
-
-/**
- * Returns the distance between two points on the Earth.
- * Direct translation from http://en.wikipedia.org/wiki/Haversine_formula
- * @param lat1d Latitude of the first point in degrees
- * @param lon1d Longitude of the first point in degrees
- * @param lat2d Latitude of the second point in degrees
- * @param lon2d Longitude of the second point in degrees
- * @return The distance between the two points in kilometers
- */
-double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
-  double lat1r, lon1r, lat2r, lon2r, u, v;
-  lat1r = deg2rad(lat1d);
-  lon1r = deg2rad(lon1d);
-  lat2r = deg2rad(lat2d);
-  lon2r = deg2rad(lon2d);
-  u = sin((lat2r - lat1r)/2);
-  v = sin((lon2r - lon1r)/2);
-  return 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
-}
-
-
-
-//complete this
-void simpleSortTotal()
+// set(index, value)		//Replaces the element at the specified index in this list with a new value.
+void linkedlist::Insert(int item)
 {
-
+    if(head == 0) 
+    {
+        head = new node; 
+        curr = head; 
+        curr->link = 0; 
+        curr->item = item; 
+    }
+    else 
+    {
+        curr->link = new node; 
+        curr = curr->link; 
+        curr->link = 0; 
+        curr->item = item; 
+    }
 }
+// size()					//Returns the number of elements in this list.
+
+while(cur != NULL)
+{
+int count;
+cur->next;
+count++
+}
+// subList(start, length)	//Returns a new list containing elements from a sub-range of this list.
+LinkedList<ValueType> sub = list.subList(start, length);
+
+// toString()				//Converts the list to a printable string representation.
+string SList::toString() const {
+    stringstream ss;
+    if (head != NULL) {
+        ss.str("");
+    } else {
+        int i = 1;
+        for (SLNode* n = head; n != NULL; n->getNextNode()) {
+            if  (i < (size - 1))
+                ss << n->getContents() << ", ";
+            ss << n->getContents();
+            i++;
+        }
+    }
+    return ss.str();
 
